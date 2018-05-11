6345681d642300d107bdfcfec6a0672a1f7f5b8b
Petra <petraol@kth.se>
2018-05-12 00:22:38 +0200

    init ready

--- script_src.js
+++ script_src.js
@@ -1,6 +1,15 @@
 //add script here
-function createPlace(username, picture, description, name, tags, location_lat, location_long) {
-    console.log(name);
+currentUser = null;
+
+function createPlace() {
+    username = currentUser;
+    picture = document.getElementById("picture").value;
+    description = document.getElementById("description").value;
+    name = document.getElementById("name").value;
+    tags = document.getElementById("tags").value;
+    location_lat = document.getElementById("location_lat").value;
+    location_long = document.getElementById("location_long").value;
+    console.log(username, picture, description, name, tags, location_lat, location_long)
     firebase.database().ref('locations/' + name).set({
         username: username,
         picture: picture,
@@ -9,23 +18,47 @@ function createPlace(username, picture, description, name, tags, location_lat, l
         location_lat: location_lat,
         location_long: location_long
         });  
-    updateUser(username, picture, description, name, tags, location_lat, location_long);
+    updateUser(name);
+    console.log("Created new place: ", name);
 }
 
-function createUser(username) {
-    firebase.database().ref("users/" + username).set({
-            username: username,
-    });
+function createUser() {
+    username = document.getElementById('new_username').value;
+    password = document.getElementById('new_password').value;
+    allUsers = [];
+    var query = firebase.database().ref("users").orderByKey();
+    query.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var key = childSnapshot.key;
+                console.log(key);
+                allUsers.push(key);
+            });
+            console.log(allUsers);
+            if (allUsers.includes(username)) {
+                console.log("Nu stötte den på ett namn som redan finns i databasen")
+                return;
+            }
+
+            else {
+                try {
+                    firebase.database().ref('users/' + username).set({
+                        username: username,
+                        password: password
+                        });
+                    console.log("Created new user: ", username)
+                }
+                catch(err) {
+                    console.log("Fel under skapandet i databasen")
+                }
+            }
+        });  
 }
 
-function updateUser(username, picture, description, name, tags, location_lat, location_long) {
+function updateUser(name) {
   // A post entry.
   var postData = {
-            picture: picture,
-            description: description,
-            tags: tags,
-            location_lat: location_lat,
-            location_long: location_long
+            place: name
             }
 
   // Get a key for a new Post.
@@ -37,5 +70,35 @@ function updateUser(username, picture, description, name, tags, location_lat, lo
   updates['/users/' + username + '/locations'] = postData;
 
   return firebase.database().ref().update(updates);
+    console.log("Updated user locations");
 }
     
+
+function fetchInfo() {
+    username = document.getElementById('username').value;
+    password = document.getElementById('password').value;
+    firebase.database().ref('/users/' + username).on('value', function(snapshot) {
+        if (snapshot.val() != null) {
+            login(username, snapshot.val().password, password);
+            }
+        else {
+            console.log("not a user")
+        }
+    });
+}
+
+function login(username, dbpassword, password) {
+    if (dbpassword === password) {
+        setCurrentUser(username);
+        console.log("Passwords match");
+    }
+    else {
+        console.log("Fel lösenord eller användarnamn!")
+    }
+			
+}
+
+function setCurrentUser(username) {
+    currentUser = username;
+    console.log("currentUser", currentUser)
+}
